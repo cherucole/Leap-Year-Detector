ead7490c6e94fd1148d0b4d08ee89630bb0415f2
cherucole <cheruyotcheruyot@gmail.com>
2018-07-11 15:22:12 +0300

    BDD practice

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("form#leap-year").submit(function(event) {
+      event.preventDefault();
+      var year = parseInt($("input#year").val());
+      var result = leapYear(year);
+      $("#result").text(result);
+    });
+  });
+
+
+
+  var leapYear = function(year) {
+      return false;
+    };
+
+    // jQuery user interface logic here.
+    
