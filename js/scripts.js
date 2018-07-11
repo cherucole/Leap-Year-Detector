74226c9cf8219f4baf161fdf7033c9cb960f3d62
cherucole <cheruyotcheruyot@gmail.com>
2018-07-11 15:34:50 +0300

    combined both logics to avoid redundancy

--- js/scripts.js
+++ js/scripts.js
@@ -15,13 +15,12 @@ $(document).ready(function() {
 
     // jQuery user interface logic here.
 
-
     var leapYear = function(year) {
-    if (year % 4 === 0) {
-      return true;
-    } else {
-      return false;
-    }
-  };
+      if ((year % 4 === 0) && (year % 100 !== 0)) {
+        return true;
+      } else {
+        return false;
+      }
+    };
 
-  // jQuery user interface logic here.
+    // jQuery user interface logic here.
